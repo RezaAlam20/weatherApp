cf6186f08b2fdf504205c9ffdeb95f401bcae25d
RezaAlam20 <rzaalam384@gmail.com>
2025-11-25 23:23:19 +0330

    fixed problem with static url moved it in to the function exported the functions lines 7/18-20

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import './dom.js';
 
 const api_key = 'APW8UZG2P23V9HBML6PTFQAJG';
-let city = 'london';
-const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${api_key}`;
+let city;
+
 async function getWeatherData() {
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${api_key}`;
   const data = await fetch(url);
   const parsed = await data.json();
   const today = parsed.days[0];
-  const description = today.description;
-  const temp = today.temp;
-  return { description, temp };
+
+  return today;
 }
 
 function selectCity(cityName) {
   city = cityName;
 }
+
+export { getWeatherData };
+export { selectCity };
