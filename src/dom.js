7050d6c279bbd1c70f776d155a56543a96254f34
RezaAlam20 <rzaalam384@gmail.com>
2025-11-25 23:24:07 +0330

    added dom weather card and html to display the weather lines 10-32

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,32 @@
+import { getWeatherData } from './index.js';
+import { selectCity } from './index.js';
+
 const content = document.querySelector('#content');
 const submit = document.querySelector('#submitBtn');
 const input = document.querySelector('#name');
+let weatherData;
 let city;
-submit.addEventListener('click', (e) => {
+submit.addEventListener('click', async (e) => {
   e.preventDefault();
   city = input.value;
+  selectCity(city);
+  weatherData = await getWeatherData();
+  makeWeatherSheet(weatherData);
+  console.log(weatherData);
 });
+
+function makeWeatherSheet(data) {
+  while (content.firstElementChild) {
+    content.removeChild(content.firstElementChild);
+  }
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <h1 class="cityName">${city}</h1>
+    <p class="info temp">temprature : ${data.temp}</p>
+    <p class="info description">description : ${data.description}</p>
+    <p class="info sunrise">sunrise : ${data.sunrise}</p>
+    <p class="info sunset">sunset :${data.sunset}</p>
+    <p class="info windspeed">windspeed :${data.windspeed}</p>`;
+  content.appendChild(div);
+  document.querySelector('.cityName').value;
+}
